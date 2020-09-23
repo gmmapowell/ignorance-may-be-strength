a6f2671909cf9e9b635ddaea9daf843bb94a20b5
Gareth Powell <gareth@ziniki.org>
2020-09-23 07:18:26 -0400

    basic syntax highlighting

---
+++ vscode-java/src/extension.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { workspace, languages, window, commands, ExtensionContext, Disposable } from 'vscode';
+
+export function activate(context: ExtensionContext) {
+}
