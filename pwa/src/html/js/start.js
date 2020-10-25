6940c4b11312ea280af7087523254ecd2b10fd32
Gareth Powell <gareth@ziniki.org>
2020-10-25 08:43:21 -0400

    Added a service worker

---
+++ pwa/src/html/js/start.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/js/service-worker.js')
+		.then((registration) => {
+			console.log("have registered service worker", registration);
+			registration.update().catch(ex => console.log(ex.message));
+		}).catch(ex => {
+			console.log("failed to register service worker", ex.message);
+		});
+	});
+}
