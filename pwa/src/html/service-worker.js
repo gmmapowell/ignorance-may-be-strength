bb9aa39026a3115d2d85dbfdd15bc1d0c3cdb92d
Gareth Powell <gareth@ziniki.org>
2020-10-29 16:44:02 -0400

    Added fetch event handler

---
+++ pwa/src/html/service-worker.js
@@ -0,0 +1,30 @@
+console.log("hello from the service worker");
+
+self.addEventListener('fetch', function(ev) {
+	var url = ev.request.url;
+	console.log('fetch seen for ' + url);
+	ev.respondWith(
+		caches.match(ev.request)
+		.then(function(response) {
+			if (response)
+				return response;
+			
+			return fetch(ev.request).then(
+				function(response) {
+					// handle error cases that we wouldn't want to cache
+					if (!response || response.status !== 200 || response.type !== 'basic')
+						return response;
+
+					// clone the response and cache it
+					var clonedResponse = response.clone();
+					caches.open("simple")
+					.then(function(cache) {
+						cache.put(ev.request, clonedResponse);
+					}).catch(err => console.log("error opening simple cache", err.message));
+
+					return response;
+				}
+			).catch(err => console.log("error", err.message, ev.request.url));
+		}).catch(err => console.log("failed to fetch", ev.request.url, err.message))
+	)
+});
