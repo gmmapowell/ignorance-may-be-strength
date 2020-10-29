96ec3831df1e0bc9372d90d9a3917a546264b787
Gareth Powell <gareth@ziniki.org>
2020-10-29 17:17:35 -0400

    Added A2HS handler

--- pwa/src/html/js/start.js
+++ pwa/src/html/js/start.js
@@ -15,5 +15,18 @@ var a2hsEv = null;
 window.addEventListener('beforeinstallprompt', function(ev) {
 	console.log("before install prompt");
 	a2hsEv = ev;
-	document.querySelector('.a2hs').style.display = 'block';
+	var btn = document.querySelector('.a2hs');
+	btn.style.display = 'block';
+	btn.addEventListener('click', function(bev) {
+		btn.style.display = 'none';
+		a2hsEv.prompt();
+		a2hsEv.userChoice.then(function(result) {
+			if (result.outcome == 'accepted') {
+				console.log("added to home screen");
+			} else {
+				console.log("not added to home screen");
+			}
+			a2hsEv = null;
+		});
+	});
 });
