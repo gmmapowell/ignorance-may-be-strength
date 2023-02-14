d2a62d00e916483b3df29f93efde339f65315377
Gareth Powell <gareth@ziniki.org>
2023-02-14 14:33:29 +0000

    Used Chromium

--- playwright-fbar/src/main/java/ignorance/FBAR.java
+++ playwright-fbar/src/main/java/ignorance/FBAR.java
@@ -1,15 +1,19 @@
 package ignorance;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 class FBAR {
 	public static void main(String[] argv) {
 		try (Playwright playwright = Playwright.create()) {
-		      Browser browser = playwright.webkit().launch();
-		      Page page = browser.newPage();
-		      page.navigate("http://whatsmyuseragent.org/");
+			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+			Page page = browser.newPage();
+			page.navigate("https://bsaefiling1.fincen.treas.gov/lc/content/xfaforms/profiles/htmldefault.html");
+			Thread.sleep(1000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
 		}
 	}
 }
