f81cd873cf4ccbb40092e73e7ad4a51a027e9b30
Gareth Powell <gareth@ziniki.org>
2023-02-14 14:54:42 +0000

    Filled in Mickey's details

--- playwright-fbar/src/main/java/ignorance/FBAR.java
+++ playwright-fbar/src/main/java/ignorance/FBAR.java
@@ -11,6 +11,11 @@ class FBAR {
 			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
 			Page page = browser.newPage();
 			page.navigate("https://bsaefiling1.fincen.treas.gov/lc/content/xfaforms/profiles/htmldefault.html");
+			page.fill("div.Email input", "mickey.mouse@disney.com");
+			page.fill("div.ConfirmedEmail input", "mickey.mouse@disney.com");
+			page.fill("div.FirstName input", "Mickey");
+			page.fill("div.LastName input", "Mouse");
+			page.fill("div.PhoneNumber input", "770-555-1234");
 			Thread.sleep(1000);
 		} catch (InterruptedException ex) {
 			ex.printStackTrace();
