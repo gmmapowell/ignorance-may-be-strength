1a48b5902ec3fea8974ad82fa053148603d8ed49
Gareth Powell <gareth@ziniki.org>
2023-02-14 15:35:38 +0000

    Refactored to use locators and added click()

--- playwright-fbar/src/main/java/ignorance/FBAR.java
+++ playwright-fbar/src/main/java/ignorance/FBAR.java
@@ -4,19 +4,23 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 
 class FBAR {
 	public static void main(String[] argv) {
 		try (Playwright playwright = Playwright.create()) {
-			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500));
 			Page page = browser.newPage();
 			page.navigate("https://bsaefiling1.fincen.treas.gov/lc/content/xfaforms/profiles/htmldefault.html");
-			page.fill("div.Email input", "mickey.mouse@disney.com");
-			page.fill("div.ConfirmedEmail input", "mickey.mouse@disney.com");
-			page.fill("div.FirstName input", "Mickey");
-			page.fill("div.LastName input", "Mouse");
-			page.fill("div.PhoneNumber input", "770-555-1234");
-			Thread.sleep(1000);
+			page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter your email address.").setExact(true)).fill("mickey.mouse@disney.com");
+			page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Re-enter your email address.")).fill("mickey.mouse@disney.com");
+			page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter your first name.")).fill("Mickey");
+			page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter your last name.")).fill("Mouse");
+			page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter your telephone number. Do not include formatting such as spaces, dashes, or other punctuation.")).fill("770-555-1234");
+			
+			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Please click this button to begin preparing your FBAR.")).click();
+			
+			Thread.sleep(10000);
 		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
