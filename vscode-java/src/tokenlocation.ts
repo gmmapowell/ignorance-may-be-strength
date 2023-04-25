ceb71a9567c3bfbb008646ff268ac87e8f505aee
Gareth Powell <gareth@ziniki.org>
2023-04-25 18:45:47 +1200

    Supported nested tokens in the view

--- vscode-java/src/tokenlocation.ts
+++ vscode-java/src/tokenlocation.ts
@@ -1,7 +1,35 @@
 import { TreeItem, TreeItemCollapsibleState, WorkspaceFolder } from "vscode";
 
 export class ProjectTokens extends TreeItem {
-	constructor(wf : WorkspaceFolder) {
+	tokens: Token[];
+	constructor(wf : WorkspaceFolder, tokens : Token[]) {
 		super(wf.name, TreeItemCollapsibleState.Collapsed);
+		this.tokens = tokens;
+	}
+
+	children() : Promise<Token[]> {
+		return Promise.resolve(this.tokens);
 	}
 }
+
+export class Token extends TreeItem {
+	locations: TokenLocation[];
+	constructor(name: string, locations: TokenLocation[]) {
+		super(name, TreeItemCollapsibleState.Collapsed);
+		this.locations = locations;
+	}
+
+	children() : Promise<TokenLocation[]> {
+		return Promise.resolve(this.locations);
+	}
+};
+
+export class TokenLocation extends TreeItem {
+	constructor(where: string) {
+		super(where, TreeItemCollapsibleState.None);
+	}
+
+	children() : Promise<Token[]> {
+		return Promise.resolve([]);
+	}
+};
--- vscode-java/src/tokensprovider.ts
+++ vscode-java/src/tokensprovider.ts
@@ -1,32 +1,44 @@
 import { Event, ProviderResult, TreeDataProvider, TreeItem } from "vscode";
-import { ProjectTokens } from "./tokenlocation";
+import { ProjectTokens, Token, TokenLocation } from "./tokenlocation";
 import * as vscode from 'vscode';
 
-export class TokensProvider implements TreeDataProvider<ProjectTokens> {
+export class TokensProvider implements TreeDataProvider<ProjectTokens | Token | TokenLocation> {
 	locations: ProjectTokens[];
 	constructor() {
 		this.locations = [];
 		if (vscode.workspace.workspaceFolders == null)
 			return;
+		const tmp = [
+			new Token("List", [
+				new TokenLocation("46.2"),
+				new TokenLocation("53.1")
+			]),
+			new Token("Map", [
+				new TokenLocation("15.7"),
+				new TokenLocation("28.9")
+			])
+		];
 		for (var wf=0;wf<vscode.workspace.workspaceFolders.length;wf++) {
-			this.locations.push(new ProjectTokens(vscode.workspace.workspaceFolders[wf]));
+			this.locations.push(new ProjectTokens(vscode.workspace.workspaceFolders[wf], tmp));
 		}
 	}
 
-	onDidChangeTreeData?: Event<ProjectTokens | null | undefined> | undefined;
-	getChildren(element?: ProjectTokens | undefined): ProviderResult<ProjectTokens[]> {
+	onDidChangeTreeData?: Event<ProjectTokens | Token | TokenLocation | null | undefined> | undefined;
+	getChildren(element?: ProjectTokens | Token | TokenLocation | undefined): ProviderResult<ProjectTokens[] | Token[] | TokenLocation[]> {
 		if (!element) { // it wants the top list
 			if (!this.locations) {
 				return Promise.resolve([]);
 			} else {
 				return Promise.resolve(this.locations);
 			}
+		} else {
+			return element.children();
 		}
 	}
-	getTreeItem(element: ProjectTokens): TreeItem {
+	getTreeItem(element: ProjectTokens | Token | TokenLocation): TreeItem {
 		return element;
 	}
-	getParent?(element: ProjectTokens): ProviderResult<ProjectTokens> {
+	getParent?(element: ProjectTokens | Token | TokenLocation): ProviderResult<ProjectTokens | Token | TokenLocation> {
 		throw new Error("Method not implemented.");
 	}
 }
