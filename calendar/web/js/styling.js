e1be9a8b74230f0023d161497e77e4831e09274a
Gareth Powell <gareth@ziniki.org>
2023-07-19 17:02:33 +0100

    Correctly account for the borders in calculations

--- calendar/web/js/styling.js
+++ calendar/web/js/styling.js
@@ -1,6 +1,7 @@
 var styledDiv;
 var sheet;
 var controlPane;
+var borderX = 1, borderY = 1;
 
 function initStyling(fbdiv) {
 	styledDiv = fbdiv;
@@ -19,13 +20,13 @@ function fitToPageSize(rows) {
 	var unitIn = "px";
 
 	// calculate desired box sizes
-	var xunit = Math.floor(pageSize.x / 91);
+	var xunit = (pageSize.x - 14 * borderX) / 91;
 	var xday = xunit*12;
 	var xmargin = xunit/2;
 	var xpos = xday / 5;
 	var xsize = xday / 8;
 
-	var yunit = Math.floor(pageSize.y / (rows * 13));
+	var yunit = (pageSize.y - borderY * 2 * rows)/ (rows * 13);
 	var yweek = yunit*12;
 	var ymargin = yunit/2;
 	var ypos = yweek / 5;
@@ -34,8 +35,8 @@ function fitToPageSize(rows) {
 	var dateSize = Math.min(xsize, ysize);
 
 	// generate new rules
-	sheet.insertRule(".feedback { border-width: 0.1mm; width: " + pageSize.x + "px; height: " + pageSize.y + "px; }");
-	sheet.insertRule(".body-day { border-width: 0.1mm; width: " + xday + unitIn + "; height: " + yweek + unitIn + "; margin: " + ymargin + unitIn + " " + xmargin + unitIn + " }");
+	sheet.insertRule(".feedback { border-width: " + borderY + unitIn + " " + borderX + unitIn +"; width: " + pageSize.x + "px; height: " + pageSize.y + "px; }");
+	sheet.insertRule(".body-day { border-width: " + borderY + unitIn + " " + borderX + unitIn +"; width: " + xday + unitIn + "; height: " + yweek + unitIn + "; margin: " + ymargin + unitIn + " " + xmargin + unitIn + " }");
 	sheet.insertRule(".body-day-date { top: " + ypos + unitIn + "; left: " + xpos + unitIn + "; font-size: " + dateSize + unitIn + " }");
 }
 
@@ -43,8 +44,8 @@ function calculateSizeOfFeedbackDiv() {
 	var viewx = window.innerWidth;
 	var viewy = window.innerHeight;
 
-	var fbx = viewx - 16 - 5; // 16 for double body margin // allow, say, 5 for border
-	var fby = viewy - controlPane.clientHeight - 16 - 5;
+	var fbx = viewx - 16 - borderX * 2; // 16 for double body margin
+	var fby = viewy - controlPane.clientHeight - 16 - borderY * 2;
 
 	return { x : fbx, y : fby };
 }
