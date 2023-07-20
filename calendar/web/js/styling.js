08ba89471edb2f93b3acbb3eab3c32bfa938e620
Gareth Powell <gareth@ziniki.org>
2023-07-20 07:50:39 +0100

    Extracted pageLayout from fitToPageSize to handle screen case

--- calendar/web/js/styling.js
+++ calendar/web/js/styling.js
@@ -1,7 +1,6 @@
 var styledDiv;
 var sheet, printSheet;
 var controlPane;
-var borderX = 1, borderY = 1;
 
 function initStyling(fbdiv) {
 	styledDiv = fbdiv;
@@ -12,22 +11,22 @@ function initStyling(fbdiv) {
 }
 
 function fitToPageSize(rows) {
-	// delte the old rules
+	pageLayout(sheet, rows, calculateSizeOfFeedbackDiv());
+}
+
+function pageLayout(sheet, rows, pageSize) {
+	// delete the old rules
 	while (sheet.cssRules.length > 0)
 		sheet.deleteRule(0);
 
-	// calculate the page size
-	var pageSize = calculateSizeOfFeedbackDiv();
-	var unitIn = "px";
-
 	// calculate desired box sizes
-	var xunit = (pageSize.x - 14 * borderX) / 91;
+	var xunit = (pageSize.x - 14 * pageSize.borderX) / 91;
 	var xday = xunit*12;
 	var xmargin = xunit/2;
 	var xpos = xday / 5;
 	var xsize = xday / 8;
 
-	var yunit = (pageSize.y - borderY * 2 * rows)/ (rows * 13);
+	var yunit = (pageSize.y - pageSize.borderY * 2 * rows)/ (rows * 13);
 	var yweek = yunit*12;
 	var ymargin = yunit/2;
 	var ypos = yweek / 5;
@@ -36,17 +35,18 @@ function fitToPageSize(rows) {
 	var dateSize = Math.min(xsize, ysize);
 
 	// generate new rules
-	sheet.insertRule(".feedback { border-width: " + borderY + unitIn + " " + borderX + unitIn +"; width: " + pageSize.x + "px; height: " + pageSize.y + "px; }");
-	sheet.insertRule(".body-day { border-width: " + borderY + unitIn + " " + borderX + unitIn +"; width: " + xday + unitIn + "; height: " + yweek + unitIn + "; margin: " + ymargin + unitIn + " " + xmargin + unitIn + " }");
-	sheet.insertRule(".body-day-date { top: " + ypos + unitIn + "; left: " + xpos + unitIn + "; font-size: " + dateSize + unitIn + " }");
+	sheet.insertRule(".feedback { border-width: " + pageSize.borderY + pageSize.unitIn + " " + pageSize.borderX + pageSize.unitIn +"; width: " + pageSize.x + pageSize.unitIn + "; height: " + pageSize.y + pageSize.unitIn + "; }");
+	sheet.insertRule(".body-day { border-width: " + pageSize.borderY + pageSize.unitIn + " " + pageSize.borderX + pageSize.unitIn +"; width: " + xday + pageSize.unitIn + "; height: " + yweek + pageSize.unitIn + "; margin: " + ymargin + pageSize.unitIn + " " + xmargin + pageSize.unitIn + " }");
+	sheet.insertRule(".body-day-date { top: " + ypos + pageSize.unitIn + "; left: " + xpos + pageSize.unitIn + "; font-size: " + dateSize + pageSize.unitIn + " }");
 }
 
 function calculateSizeOfFeedbackDiv() {
+	var borderX = 1, borderY = 1;
 	var viewx = window.innerWidth;
 	var viewy = window.innerHeight;
 
 	var fbx = viewx - 16 - borderX * 2; // 16 for double body margin
 	var fby = viewy - controlPane.clientHeight - 16 - borderY * 2;
 
-	return { x : fbx, y : fby };
+	return { x : fbx, y : fby, unitIn: "px", borderX, borderY };
 }
