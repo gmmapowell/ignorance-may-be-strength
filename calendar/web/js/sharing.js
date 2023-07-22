a2691d5e7d2ea9c4107cf9774509685eaa74c1a6
Gareth Powell <gareth@ziniki.org>
2023-07-22 09:22:13 +0100

    loading a shared json from a file

--- calendar/web/js/sharing.js
+++ calendar/web/js/sharing.js
@@ -1,4 +1,7 @@
+var sharingFile;
+
 function initSharing() {
+	sharingFile = document.getElementById('sharing-file');
 }
 
 function shareJson() {
@@ -12,4 +15,21 @@ function assemblePropertiesObject() {
 		firstDay: first.value,
 		calendars: Object.keys(calendars)
 	};
+}
+
+function loadJsonFromFile() {
+	sharingFile.files[0].text().then(tx => configureWith(JSON.parse(tx)));
+}
+
+function configureWith(json) {
+	start.value = json.start;
+	end.value = json.end;
+	first.value = json.firstDay;
+	for (var i=0;i<json.calendars.length;i++) {
+		loadCalendar(json.calendars[i]);
+	}
+}
+
+function loadCalendar(url) {
+	ajax(url, (status, response) => handleICS(url, status, response));
 }
