d048408bfb9b05e4ed30a9949dfefde1aa500bac
Gareth Powell <gareth@ziniki.org>
2024-01-01 13:08:56 -0500

    Separated out the initialization in the allocator

--- homer_rust/src/allocator.rs
+++ homer_rust/src/allocator.rs
@@ -1,38 +1,22 @@
 mod tests;
+mod init_alloc;
 
 use core::alloc::GlobalAlloc;
 use core::cell::UnsafeCell;
 
-extern {
-    pub static __heap_start : u32;
-    pub static __heap_end : u32;
-}
-
-#[cfg(not(test))]
-#[allow(non_upper_case_globals)]
-#[no_mangle]
-pub static __rust_no_alloc_shim_is_unstable: u8 = 0;
-
-#[cfg(not(test))]
-#[global_allocator]
-static HEAP: PageAllocator = PageAllocator {
-    next_page: UnsafeCell::new(0),
-    free_16: UnsafeCell::new(0),
-    free_256: UnsafeCell::new(0),
-    free_4096: UnsafeCell::new(0)
-};
-
-struct PageAllocator {
+struct PageAllocator<F> where F: Fn() -> (usize,usize) {
     next_page: UnsafeCell<usize>,
+    end: UnsafeCell<usize>,
     free_16: UnsafeCell<usize>,
     free_256: UnsafeCell<usize>,
-    free_4096: UnsafeCell<usize>
+    free_4096: UnsafeCell<usize>,
+    init: F
 }
 
 // OK to hack this because we are single threaded for now
-unsafe impl Sync for PageAllocator {}
+unsafe impl<F> Sync for PageAllocator<F> where F: Fn() -> (usize,usize) {}
 
-unsafe impl GlobalAlloc for PageAllocator {
+unsafe impl<F> GlobalAlloc for PageAllocator<F> where F: Fn() -> (usize,usize) {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
         if layout.size() <= 16 && layout.align() <= 16 {
             self.next(&self.free_16)
@@ -50,16 +34,18 @@ unsafe impl GlobalAlloc for PageAllocator {
     }
 }
 
-impl PageAllocator {
+impl<F> PageAllocator<F> where F: Fn() -> (usize,usize) {
     unsafe fn next(&self, list: &UnsafeCell<usize>) -> *mut u8 {
         let lp: *mut usize = list.get();
         if *lp == 0 {
             let np = self.next_page.get();
             // we don't have any free memory, allocate next page
             if *np == 0 {
-                *np = (&__heap_start as *const _) as usize;
+                let (start, end) = (self.init)();
+                *np = start;
+                *self.end.get() = end;
             }
-            if *np >= (&__heap_end as *const _) as usize {
+            if *np >= *self.end.get() {
                 return core::ptr::null_mut(); // we are out of memory
             }
 
--- homer_rust/src/allocator/tests.rs
+++ homer_rust/src/allocator/tests.rs
@@ -1,28 +1,31 @@
 #[cfg(test)]
 mod tests {
-    #[no_mangle]
-    pub static __heap_start :u32 = 0;
-    #[no_mangle]
-    pub static __heap_end :u32 = 0;
-
     use crate::allocator::PageAllocator;
     use core::{cell::UnsafeCell, alloc::GlobalAlloc};
+    use alloc::boxed::Box;
 
     #[test]
     fn test_allocate_first_page() {
-        let pa = simple_allocator();
+        let (start, pa) = simple_allocator();
         unsafe {
             let addr = pa.alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap()) as usize;
-            assert_eq!(addr, 4096);
+            assert_eq!(addr, start);
         }
     }
 
-    fn simple_allocator() -> PageAllocator {
-        return PageAllocator{
-            next_page: UnsafeCell::new(0),
-            free_16: UnsafeCell::new(0),
-            free_256: UnsafeCell::new(0),
-            free_4096: UnsafeCell::new(0)        
+    fn simple_allocator() -> (usize, PageAllocator<Box<dyn Fn() -> (usize,usize)>>) {
+        unsafe {
+            let blk = alloc::alloc::alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap());
+            let start = (blk as *const _) as usize;
+            let f = move || { (start, start+4096) };
+            (start, PageAllocator{
+                next_page: UnsafeCell::new(0),
+                end: UnsafeCell::new(0),
+                free_16: UnsafeCell::new(0),
+                free_256: UnsafeCell::new(0),
+                free_4096: UnsafeCell::new(0),
+                init: Box::new(f)
+            })
         }
     }
 }
