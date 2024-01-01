e77221c5fd2c925b6fc92813c18dbaf7268fb235
Gareth Powell <gareth@ziniki.org>
2024-01-01 15:23:33 -0500

    Handling the simple shorter allocation cases

--- homer_rust/src/allocator.rs
+++ homer_rust/src/allocator.rs
@@ -19,11 +19,11 @@ unsafe impl<F> Sync for PageAllocator<F> where F: Fn() -> (usize,usize) {}
 unsafe impl<F> GlobalAlloc for PageAllocator<F> where F: Fn() -> (usize,usize) {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
         if layout.size() <= 16 && layout.align() <= 16 {
-            self.next(&self.free_16)
+            self.next(&self.free_16, 16)
         } else if layout.size() <= 256 && layout.align() <= 256 {
-            self.next(&self.free_256)
+            self.next(&self.free_256, 256)
         } else if layout.size() <= 4096 && layout.align() <= 4096 {
-            self.next(&self.free_4096)
+            self.next(&self.free_4096, 4096)
         } else {
             core::ptr::null_mut() // this is allegedly what will cause OOM exceptions
         }
@@ -35,7 +35,7 @@ unsafe impl<F> GlobalAlloc for PageAllocator<F> where F: Fn() -> (usize,usize) {
 }
 
 impl<F> PageAllocator<F> where F: Fn() -> (usize,usize) {
-    unsafe fn next(&self, list: &UnsafeCell<usize>) -> *mut u8 {
+    unsafe fn next(&self, list: &UnsafeCell<usize>, size: usize) -> *mut u8 {
         let lp: *mut usize = list.get();
         if *lp == 0 {
             let np = self.next_page.get();
@@ -49,12 +49,35 @@ impl<F> PageAllocator<F> where F: Fn() -> (usize,usize) {
                 return core::ptr::null_mut(); // we are out of memory
             }
 
-            // TODO: still need to initialize inside of block if not 4096
-            *lp = *np;
+            self.init_block(*np as *mut u8, list, size);
             *np = *np + 4096;
         }
         let curr = *lp;
         *lp = *(curr as *const usize);
         curr as *mut u8
+    }
+
+    fn init_block(&self, blk: *mut u8, list: &UnsafeCell<usize>, size: usize) {
+        unsafe {
+            if size == 4096 {
+                // we just write a 0 at the start of the block
+                *(blk as *mut usize) = 0 as usize;
+                *list.get() = blk as usize;
+            } else {
+                // the first word is the size of the entries in this block, which we will use on dealloc()
+                *(blk as *mut usize) = size;
+                let max = blk.add(4096);
+                // then each of the remaining words is a pointer to the next
+                let mut wp = blk.add(size);
+                *list.get() = wp as usize;
+                let mut np = wp.add(size);
+                while np < max {
+                    *(wp as *mut usize) = np as usize;
+                    wp = wp.add(size);
+                    np = np.add(size);
+                }
+                *(wp as *mut usize) = 0 as usize; // mark the final pointer as full
+            }
+        }
     }
 }
--- homer_rust/src/allocator/tests.rs
+++ homer_rust/src/allocator/tests.rs
@@ -6,18 +6,96 @@ mod tests {
 
     #[test]
     fn test_allocate_first_page() {
-        let (start, pa) = simple_allocator();
+        let (start, pa) = simple_allocator(1);
         unsafe {
             let addr = pa.alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap()) as usize;
             assert_eq!(addr, start);
         }
     }
 
-    fn simple_allocator() -> (usize, PageAllocator<Box<dyn Fn() -> (usize,usize)>>) {
+    #[test]
+    fn test_cannot_allocate_second_page_when_only_one() {
+        let (_, pa) = simple_allocator(1);
         unsafe {
-            let blk = alloc::alloc::alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap());
+            let _ = pa.alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap()) as usize;
+            let addr2 = pa.alloc(alloc::alloc::Layout::from_size_align(4096, 16).unwrap()) as usize;
+            assert_eq!((core::ptr::null_mut() as *const u8) as usize, addr2);
+        }
+    }
+
+    #[test]
+    fn test_allocate_16_bytes_is_16_beyond_start() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let addr = pa.alloc(alloc::alloc::Layout::from_size_align(16, 16).unwrap()) as usize;
+            assert_eq!(addr, start+16);
+        }
+    }
+
+    #[test]
+    fn test_we_can_allocate_16_bytes_255_times() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let mut i = 1;
+            while i < 256 {
+                let addr: usize = pa.alloc(alloc::alloc::Layout::from_size_align(16, 16).unwrap()) as usize;
+                assert_eq!(addr, start+16*i, "i={}", i);
+                i = i+1;
+            }
+            // test that a further allocation will be OOM
+            let will_be_null: usize = pa.alloc(alloc::alloc::Layout::from_size_align(16, 16).unwrap()) as usize;
+            assert_eq!(will_be_null as *const u8, core::ptr::null());
+        }
+    }
+
+    #[test]
+    fn test_allocate_256_bytes_is_256_beyond_start() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let addr = pa.alloc(alloc::alloc::Layout::from_size_align(256, 256).unwrap()) as usize;
+            assert_eq!(addr, start+256);
+        }
+    }
+
+    #[test]
+    fn test_we_can_allocate_256_bytes_15_times() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let mut i = 1;
+            while i < 16 {
+                let addr: usize = pa.alloc(alloc::alloc::Layout::from_size_align(256, 256).unwrap()) as usize;
+                assert_eq!(addr, start+256*i, "i={}", i);
+                i = i+1;
+            }
+            // test that a further allocation will be OOM
+            let will_be_null: usize = pa.alloc(alloc::alloc::Layout::from_size_align(256, 256).unwrap()) as usize;
+            assert_eq!(will_be_null as *const u8, core::ptr::null());
+        }
+    }
+
+    #[test]
+    fn test_allocating_10_bytes_is_the_same_as_16() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let addr = pa.alloc(alloc::alloc::Layout::from_size_align(10, 4).unwrap()) as usize;
+            assert_eq!(addr, start+16);
+        }
+    }
+
+    #[test]
+    fn test_allocating_17_bytes_is_the_same_as_256() {
+        let (start, pa) = simple_allocator(1);
+        unsafe {
+            let addr = pa.alloc(alloc::alloc::Layout::from_size_align(17, 8).unwrap()) as usize;
+            assert_eq!(addr, start+256);
+        }
+    }
+
+    fn simple_allocator(npages: usize) -> (usize, PageAllocator<Box<dyn Fn() -> (usize,usize)>>) {
+        unsafe {
+            let blk = alloc::alloc::alloc(alloc::alloc::Layout::from_size_align(4096 * npages, 16).unwrap());
             let start = (blk as *const _) as usize;
-            let f = move || { (start, start+4096) };
+            let f = move || { (start, start+4096 * npages) };
             (start, PageAllocator{
                 next_page: UnsafeCell::new(0),
                 end: UnsafeCell::new(0),
