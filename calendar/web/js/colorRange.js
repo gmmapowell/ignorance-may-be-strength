d348b3bad94cf3531845eb3d3c9273a4af58f754
Gareth Powell <gareth@ziniki.org>
2024-01-17 15:42:25 +0000

    Fixing issues with color ranges and UTC dates

--- calendar/web/js/colorRange.js
+++ calendar/web/js/colorRange.js
@@ -1,17 +1,17 @@
 function colorRange(range) {
-	console.log(range);
-	var from = new Date(Date.parse(range.from + "Z"));
-	var to = new Date(Date.parse(range.to + "Z"));
+	// console.log(range);
+	var from = new Date(Date.parse(range.from));
+	var to = new Date(Date.parse(range.to));
 	var color = range.color;
 	var label = range.label;
 	var curr = from;
 	do {
-		console.log(curr);
-		var date = curr.getFullYear() + "-" + (curr.getMonth()+1).toString().padStart(2, '0') + "-" + curr.getDate().toString().padStart(2, '0');
+		// console.log(curr);
+		var date = curr.getUTCFullYear() + "-" + (curr.getUTCMonth()+1).toString().padStart(2, '0') + "-" + curr.getUTCDate().toString().padStart(2, '0');
 		if (!colors[date]) {
 			colors[date] = [];
 		}
 		colors[date].push({ color, label });
-		curr.setDate(curr.getDate()+1);
+		curr.setUTCDate(curr.getUTCDate()+1);
 	} while (curr <= to);
 }
