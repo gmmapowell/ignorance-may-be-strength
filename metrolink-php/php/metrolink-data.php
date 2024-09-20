5eaeb81daa3b749da93c3a81343a7b9a9297b00e
Gareth Powell <gareth@ziniki.org>
2024-09-20 17:59:47 +0100

    Outline of transform()

--- metrolink-php/php/metrolink-data.php
+++ metrolink-php/php/metrolink-data.php
@@ -1,8 +1,13 @@
 <?php
   require('metrolink_key.php');
+  require('transform.php');
 
   $ch = curl_init("https://api.tfgm.com/odata/Metrolinks");
   curl_setopt($ch, CURLOPT_HTTPHEADER, array($metrolink_key));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec($ch);
+
+  $data = json_decode($response);
+  $transformed = transform($data->value);
+  print json_encode($transformed);
 ?>
