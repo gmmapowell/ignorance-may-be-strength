132504ca3a13b38b15950c2194f95491bbed0023
Gareth Powell <gareth@ziniki.org>
2024-09-21 12:35:16 +0100

    Get first test passing

--- metrolink-php/php/metrolink-data.php
+++ metrolink-php/php/metrolink-data.php
@@ -2,12 +2,14 @@
   require('metrolink_key.php');
   require('transform.php');
 
+  $transformer = new Transformer($_GET);
+
   $ch = curl_init("https://api.tfgm.com/odata/Metrolinks");
   curl_setopt($ch, CURLOPT_HTTPHEADER, array($metrolink_key));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec($ch);
 
   $data = json_decode($response);
-  $transformed = transform($data->value);
+  $transformed = $transformer->transform($data->value);
   print json_encode($transformed);
 ?>
--- metrolink-php/php/transform.php
+++ metrolink-php/php/transform.php
@@ -1,5 +1,14 @@
 <?php
-  function transform($odata) {
-    return array("Firswood" => array("Victoria" => ["19:59", "20:09"]));
+
+  class Transformer {
+    var $from, $to;
+    public function __construct($params) {
+      $this->from = array_key_exists('from', $params) ? $params['from'] : [];
+      $this->to = array_key_exists('to', $params) ? $params['to'] : [];
+    }
+
+    function transform($odata) {
+      return array("Firswood" => array("Victoria" => ["19:59", "20:09"]));
+    }
   }
 ?>
