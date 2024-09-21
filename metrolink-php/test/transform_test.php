fc7dd65f7d38aff7d267bacb479445148376fd83
Gareth Powell <gareth@ziniki.org>
2024-09-21 13:12:04 +0100

    A first case with actual data

--- metrolink-php/test/transform_test.php
+++ metrolink-php/test/transform_test.php
@@ -18,14 +18,25 @@ final class Transform_test extends TestCase
 
     public function test_requiring_from_fir_excludes_from_air() {
         $transformer = new Transformer(["from" => ["FIR"]]);
-        $out = $transformer->transform(["TLAREF" => "AIR"]);
+        $out = $transformer->transform([["TLAREF" => "AIR"]]);
         $this->assertEquals([], $out);
     }
 
     public function test_requiring_to_fir_excludes_to_air() {
         $transformer = new Transformer(["to" => ["FIR"]]);
-        $out = $transformer->transform(["TLAREF" => "AIR"]);
+        $out = $transformer->transform([["TLAREF" => "AIR"]]);
         $this->assertEquals([], $out);
+    }
+    
+    public function test_from_fir_includes_a_single_matching_entry() {
+        $transformer = new Transformer(["from" => ["FIR"]]);
+        $out = $transformer->transform([[
+            "TLAREF" => "FIR",
+            "Dest0" => "Victoria",
+            "Wait0" => "6",
+            "LastUpdated" => "2024-09-17T19:52:55Z"
+        ]]);
+        $this->assertEquals([["FIR" => ["VIC" => [ "19:58" ]]]], $out);
     }
 }
 ?>
