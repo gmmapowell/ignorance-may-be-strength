eefa22e87a1908f575907e63ef21bc60de44bd53
Gareth Powell <gareth@ziniki.org>
2024-09-22 08:11:57 +0100

    First couple of filtering tests

--- metrolink-php/test/transform_test.php
+++ metrolink-php/test/transform_test.php
@@ -24,10 +24,10 @@ final class Transform_test extends TestCase
 
     public function test_requiring_to_fir_excludes_to_air() {
         $transformer = new Transformer(["to" => ["FIR"]]);
-        $out = $transformer->transform([["TLAREF" => "AIR"]]);
+        $out = $transformer->transform([["Dest0" => "Manchester Airport"]]);
         $this->assertEquals([], $out);
     }
-    
+
     public function test_from_fir_includes_a_single_matching_entry() {
         $transformer = new Transformer(["from" => ["FIR"]]);
         $out = $transformer->transform([[
@@ -36,7 +36,41 @@ final class Transform_test extends TestCase
             "Wait0" => "6",
             "LastUpdated" => "2024-09-17T19:52:55Z"
         ]]);
-        $this->assertEquals([["FIR" => ["VIC" => [ "19:58" ]]]], $out);
+        $this->assertEquals(["FIR" => ["VIC" => [ "19:58" ]]], $out);
     }
-}
+
+    public function test_firswood_has_two_trams_to_Victoria() {
+        $transformer = new Transformer(["from" => ["FIR"]]);
+        $out = $transformer->transform([[
+            "TLAREF" => "FIR",
+            "Dest0" => "Victoria",
+            "Wait0" => "6",
+            "Dest2" => "Victoria",
+            "Wait2" => "18",
+            "LastUpdated" => "2024-09-17T19:52:55Z"
+        ]]);
+        $this->assertEquals(["FIR" => ["VIC" => [ "19:58", "20:10" ]]], $out);
+    }
+
+    public function test_tram_to_Altrincham_passes_with_empty_filter_in_Dest0() {
+        $transformer = new Transformer([]);
+        $matches = $transformer->filter([
+            "TLAREF" => "SPS",
+            "Dest0" => "Altrincham",
+            "Wait0" => "6",
+            "LastUpdated" => "2024-09-17T19:52:55Z"
+        ], 0);
+        $this->assertTrue($matches);
+    }
+
+    public function test_to_Altrincham_passes_filter_in_Dest0() {
+        $transformer = new Transformer(["to" => ["Altrincham"]]);
+        $matches = $transformer->filter([
+            "TLAREF" => "SPS",
+            "Dest0" => "Altrincham",
+            "Wait0" => "6",
+            "LastUpdated" => "2024-09-17T19:52:55Z"
+        ], 0);
+        $this->assertTrue($matches);
+    }}
 ?>
