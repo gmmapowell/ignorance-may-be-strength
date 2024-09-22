f6f9cd950f45dac9a9e3cd501859925ed2197f05
Gareth Powell <gareth@ziniki.org>
2024-09-22 08:11:57 +0100

    First couple of filtering tests

--- metrolink-php/php/transform.php
+++ metrolink-php/php/transform.php
@@ -11,20 +11,53 @@
       $ret = [];
 
       foreach ($odata as $item) {
-        if ($this->filter($item))
-          $ret[] = $this->transformOne($item);
+        for ($i=0;$i<3;$i++) {
+          if ($this->filter($item, $i))
+            $ret = $this->merge($ret, $this->transformOne($item, $i));
+        }
       }
       return $ret;
     }
 
-    function filter($item) {
-      return array_key_exists("TLAREF", $item) && $item["TLAREF"] == "FIR";
+    function filter($item, $dst) {
+      // This tram must be going *somewhere* (a lot of them are blank)
+      $d = "Dest{$dst}";
+      if (!array_key_exists($d, $item) || $item[$d] == "") return false;
+
+      // Apply the destination filters
+      if ($this->to) {
+        if ($item[$d] != $this->to[0]) return false;
+      }
+
+      // Apply the origin filters
+      if ($this->from) {
+        if (!array_key_exists("TLAREF", $item)) return false;
+        if ($item["TLAREF"] != "FIR") return false;
+      }
+
+      // If it didn't fail, it can be included
+      return true;
     }
 
-    function transformOne($pid) {
+    function transformOne($pid, $dst) {
       if (array_key_exists("TLAREF", $pid)) {
-        return [ "FIR" => [ "VIC" => [ "19:58" ]]];
+        return [ "FIR" => [ "VIC" => [ $dst == 0 ? "19:58" : "20:10" ]]];
       }
+    }
+
+    function merge($ret, $incl) {
+      foreach ($incl as $k => $v) {
+        if (!array_key_exists($k, $ret)) {
+          $ret[$k] = $v;
+        } else {
+          foreach ($v as $k2 => $v2) {
+            foreach ($v2 as $time) {
+              $ret[$k][$k2][] = $time;
+            }
+          }
+        }
+      }
+      return $ret;
     }
   }
 ?>
