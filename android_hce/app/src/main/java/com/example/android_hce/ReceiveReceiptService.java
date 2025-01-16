b174b842d02bbd511550126564a8169c98a10d22
Gareth Powell <gareth@ziniki.org>
2025-01-16 15:20:16 +0000

    Improve APDU tracing

--- android_hce/app/src/main/java/com/example/android_hce/ReceiveReceiptService.java
+++ android_hce/app/src/main/java/com/example/android_hce/ReceiveReceiptService.java
@@ -17,10 +17,20 @@ public class ReceiveReceiptService extends HostApduService {
 
     @Override
     public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
-       Log.w("service", "processApdu");
+       StringBuilder sb = new StringBuilder("apdu = ");
+       for (int i=0;i<apdu.length;i++) {
+           int x = ((int)apdu[i])&0xff;
+           String s = Integer.toHexString(x);
+           if (s.length() < 2)
+               sb.append("0");
+           sb.append(s);
+           sb.append(" ");
+       }
+       Log.w("service", sb.toString());
        showNotification();
        return new byte[] { (byte)0x90, 0x00 }; // APDU "OK"
     }
+
     @Override
     public void onDeactivated(int reason) {
         Log.w("service", "lost contact with card: " + reason);
