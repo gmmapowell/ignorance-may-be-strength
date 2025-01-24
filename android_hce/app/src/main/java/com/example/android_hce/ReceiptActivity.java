8d50ae94ef203ac3fe259949d8ff21863a944c44
Gareth Powell <gareth@ziniki.org>
2025-01-24 20:27:16 +0000

    Added basic receipt activity

---
+++ android_hce/app/src/main/java/com/example/android_hce/ReceiptActivity.java
@@ -0,0 +1,37 @@
+package com.example.android_hce;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.android_hce.databinding.ActivityReceiptBinding;
+
+public class ReceiptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_receipt);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
--- android_hce/app/src/main/java/com/example/android_hce/ReceiptSessionController.java
+++ android_hce/app/src/main/java/com/example/android_hce/ReceiptSessionController.java
@@ -1,8 +1,10 @@
 package com.example.android_hce;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -113,6 +115,9 @@ public class ReceiptSessionController implements SessionController {
 
     private void showNotification(long notificationId) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent intent = new Intent(context, ReceiptActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(context, ReceiptApplication.CHANNEL_ID)
@@ -120,7 +125,9 @@ public class ReceiptSessionController implements SessionController {
                         .setContentTitle("NFC")
                         .setContentText("NFC Contact Made")
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setCategory(NotificationCompat.CATEGORY_STATUS);
+                        .setCategory(NotificationCompat.CATEGORY_STATUS)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
 
         notificationManager.notify((int)notificationId, notificationBuilder.build());
     }
