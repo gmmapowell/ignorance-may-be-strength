ebc1481e15ed4d6d82546b49a409e56396a341fa
Gareth Powell <gareth@ziniki.org>
2025-02-13 11:48:18 +0000

    added minimal navigation

--- jshistory/js/history.js
+++ jshistory/js/history.js
@@ -1,4 +1,15 @@
 var logArea = document.querySelector(".logging");
+var pushid = 1;
+var version = figureVersion(window.location.pathname);
+
+function figureVersion(path) {
+	var idx = path.indexOf('/', 1);
+	if (idx == -1) {
+		return path;
+	} else {
+		return path.substring(0, idx);
+	}
+}
 
 function scrollTo(elt) {
 	logArea.scrollTo(0, elt.offsetTop + elt.offsetHeight - logArea.clientHeight);
@@ -13,10 +24,12 @@ function write(msg) {
 }
 
 function url(goto) {
-	write(goto);
+	var url = window.location;
+	url.pathname = version + '/' + goto;
+	write("pushing " + pushid + " to " + goto + ": url = " + url);
 }
 
 // export the functions that are used externally
 window.url = url;
 
-write("application loaded");
+write("application loaded: " + version);
