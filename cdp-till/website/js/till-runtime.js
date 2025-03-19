9eca6e6dc07ef7e9563eb3b9f7316a8d3822eaa6
Gareth Powell <gareth@ziniki.org>
2025-03-19 06:32:11 +0000

    Fetch /till-code

--- cdp-till/website/js/till-runtime.js
+++ cdp-till/website/js/till-runtime.js
@@ -1,19 +1,9 @@
 window.addEventListener('load', function(ev) {
-	var div = document.getElementById("content-goes-here");
-
-	var root = document.getElementById("root");
-	var iroot = root.content.cloneNode(true);
-	iroot = div.appendChild(iroot.children[0]);
-
-	var row = document.getElementById("row");
-	var irow = row.content.cloneNode(true);
-	irow = iroot.appendChild(irow.children[0]);
-
-	var cell = document.getElementById("cell");
-	var icell = cell.content.cloneNode(true);
-	icell = irow.appendChild(icell.children[0]);
-	icell.className = "cell blue-cell";
-
-	var tc = icell.querySelector(".cell-text");
-	tc.appendChild(document.createTextNode("hello"));
+	this.fetch("/till-code").then(resp => {
+		if (!resp.ok) {
+			console.log("Response err:", resp.status, resp.statusText)
+			return
+		}
+		resp.json().then(json => console.log(json));
+	});
 })
