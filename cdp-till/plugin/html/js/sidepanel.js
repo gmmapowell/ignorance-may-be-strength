364da1230c4d44ad690a12d01847be20640bf63d
Gareth Powell <gareth@ziniki.org>
2025-03-20 06:59:48 +0000

    Show Till source

--- cdp-till/plugin/html/js/sidepanel.js
+++ cdp-till/plugin/html/js/sidepanel.js
@@ -1,4 +1,9 @@
+var pre = document.getElementById("source-code");
+
 fetch("http://localhost:1399/src/cafe.till").then(resp => {
 	console.log(resp.status, resp.statusText);
-  });
+	resp.text().then(src => {
+		pre.innerText = src;
+	});
+});
   
