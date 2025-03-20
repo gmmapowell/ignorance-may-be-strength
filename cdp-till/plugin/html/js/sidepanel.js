3d0026b31b303bd9877fc2023b030d1a1390171c
Gareth Powell <gareth@ziniki.org>
2025-03-20 07:23:31 +0000

    Showing source as a table

--- cdp-till/plugin/html/js/sidepanel.js
+++ cdp-till/plugin/html/js/sidepanel.js
@@ -1,9 +1,23 @@
-var pre = document.getElementById("source-code");
+var tbody = document.getElementById("source-code");
 
 fetch("http://localhost:1399/src/cafe.till").then(resp => {
-	console.log(resp.status, resp.statusText);
 	resp.text().then(src => {
-		pre.innerText = src;
+		tbody.innerHTML = '';
+		var lines = src.split(/\r?\n/g);
+		for (var i=0;i<lines.length;i++) {
+			var tr = document.createElement("tr");
+
+			var tlineNo = document.createElement("td");
+			tlineNo.className = 'line-no'
+			tlineNo.appendChild(document.createTextNode(i+1))
+			tr.appendChild(tlineNo);
+
+			var tlineText = document.createElement("td");
+			tlineText.appendChild(document.createTextNode(lines[i]))
+			tr.appendChild(tlineText);
+
+			tbody.appendChild(tr);
+		}
 	});
 });
   
