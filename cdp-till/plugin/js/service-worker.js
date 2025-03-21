3680416057655a5e79427191e17bfbfe168ca9c8
Gareth Powell <gareth@ziniki.org>
2025-03-21 10:35:37 +0000

    Add CDP debug listener

--- cdp-till/plugin/js/service-worker.js
+++ cdp-till/plugin/js/service-worker.js
@@ -1,4 +1,9 @@
 chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
 
+chrome.debugger.onEvent.addListener(function(source, method, params) {
+    if (method == "Debugger.scriptParsed") {
+        console.log(params.url);
+    }
+});
