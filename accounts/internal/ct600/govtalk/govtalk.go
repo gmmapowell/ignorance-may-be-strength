eba272903183e35dfecd041b9ab6ae3da2dd5052
Gareth Powell <gareth@ziniki.org>
2025-05-29 17:18:40 +0100

    Finished up envelope header

--- accounts/internal/ct600/govtalk/govtalk.go
+++ accounts/internal/ct600/govtalk/govtalk.go
@@ -13,13 +13,48 @@ type GovTalkMessage struct {
 
 func (gtm *GovTalkMessage) AsXML() any {
 	env := ElementWithText("EnvelopeVersion", "2.0")
-	test := ElementWithText("GatewayTest", "1")
-	msgDetails := ElementWithNesting("MessageDetails", test)
-	sndrDetails := ElementWithNesting("SenderDetails")
-	header := ElementWithNesting("Header", msgDetails, sndrDetails)
-	utr := Key("UTR", "8596148860")
-	keys := ElementWithNesting("Keys", utr)
-	gtDetails := ElementWithNesting("GovTalkDetails", keys)
+	msgDetails := ElementWithNesting(
+		"MessageDetails",
+		ElementWithText("Class", "HMRC-CT-CT600"),
+		ElementWithText("Qualifier", "request"),
+		ElementWithText("Function", "submit"),
+		ElementWithText("Transformation", "XML"),
+		ElementWithText("GatewayTest", "1"),
+	)
+	sndrDetails := ElementWithNesting(
+		"SenderDetails",
+		ElementWithNesting(
+			"IDAuthentication",
+			ElementWithText("SenderID", "Provided by the SDST"),
+			ElementWithNesting(
+				"Authentication",
+				ElementWithText("Method", "clear"),
+				ElementWithText("Role", "Principal"),
+				ElementWithText("Value", "Provided by the SDST"),
+			),
+		),
+	)
+	gtDetails := ElementWithNesting(
+		"GovTalkDetails",
+		ElementWithNesting("Keys", Key("UTR", "8596148860")),
+		ElementWithNesting("TargetDetails", ElementWithText("Organisation", "HMRC")),
+		ElementWithNesting(
+			"ChannelRouting",
+			ElementWithNesting(
+				"Channel",
+				ElementWithText("URI", "Vendor ID"),
+				ElementWithText("Product", "Product Details"),
+				ElementWithText("Version", "Version #"),
+			),
+		),
+	)
+	return MakeGovTalkMessage(env,
+		ElementWithNesting("Header", msgDetails, sndrDetails),
+		gtDetails,
+		gtm.makeBody())
+}
+
+func (gtm *GovTalkMessage) makeBody() any {
 	body := ElementWithNesting("Body")
-	return MakeGovTalkMessage(env, header, gtDetails, body)
+	return &body
 }
