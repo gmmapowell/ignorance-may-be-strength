e602b0bb5705d5fe120f72e7852dd323d5770bb2
Gareth Powell <gareth@ziniki.org>
2025-07-08 18:41:12 +0100

    Capture CorrelationID on submit

--- accounts/internal/ct600/submission/submit.go
+++ accounts/internal/ct600/submission/submit.go
@@ -1,6 +1,10 @@
 package submission
 
 import (
+	"bytes"
+	"encoding/xml"
+	"log"
+
 	"github.com/gmmapowell/ignorance/accounts/internal/ct600/config"
 	"github.com/gmmapowell/ignorance/accounts/internal/ct600/govtalk"
 )
@@ -12,6 +16,24 @@ func Submit(conf *config.Config) error {
 		return err
 	}
 
-	_, err = transmit(send)
-	return err
+	msg, err := transmit(send)
+
+	if err != nil {
+		return err
+	}
+
+	decoder := xml.NewDecoder(bytes.NewReader(msg))
+	var data string
+	for tok, err := decoder.Token(); err == nil; tok, err = decoder.Token() {
+		switch tok := tok.(type) {
+		case xml.StartElement:
+		case xml.EndElement:
+			if tok.Name.Local == "CorrelationID" {
+				log.Printf("CorrelationID: %s\n", data)
+			}
+		case xml.CharData:
+			data = string(tok)
+		}
+	}
+	return nil
 }
