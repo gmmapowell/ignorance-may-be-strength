7d92fd2007caad1e205e9ad1d56b2b04a48deafd
Gareth Powell <gareth@ziniki.org>
2025-07-09 10:00:14 +0100

    Extracted error messages

--- accounts/internal/ct600/submission/transmit.go
+++ accounts/internal/ct600/submission/transmit.go
@@ -1,11 +1,12 @@
 package submission
 
 import (
+	"bytes"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"strings"
 )
 
 func transmit(body io.Reader) ([]byte, error) {
@@ -22,11 +23,61 @@ func transmitTo(uri string, body io.Reader) ([]byte, error) {
 	if err != nil {
 		log.Fatalf("error reading response: %v", err)
 	}
-	msg := string(respBody)
-	if strings.Contains(msg, "GovTalkErrors") {
-		fmt.Printf("%s", msg)
-		return respBody, fmt.Errorf("there was a GovTalkErrors block")
+	return respBody, handleErrors(respBody)
+}
+
+type Error struct {
+	raisedBy string
+	number   string
+	ofType   string
+	message  string
+	location string
+}
+
+func handleErrors(msg []byte) error {
+	var all []*Error
+	decoder := xml.NewDecoder(bytes.NewReader(msg))
+	var errmsg *Error
+	var data string
+	for tok, err := decoder.Token(); err == nil; tok, err = decoder.Token() {
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			if tok.Name.Local == "Error" {
+				errmsg = &Error{}
+				all = append(all, errmsg)
+			}
+		case xml.EndElement:
+			if tok.Name.Local == "Error" {
+				errmsg = nil
+			} else if errmsg != nil {
+				switch tok.Name.Local {
+				case "RaisedBy":
+					errmsg.raisedBy = data
+				case "Number":
+					errmsg.number = data
+				case "Type":
+					errmsg.ofType = data
+				case "Text":
+					errmsg.message = data
+				case "Location":
+					errmsg.location = data
+				}
+			}
+		case xml.CharData:
+			data = string(tok)
+		default:
+			// log.Printf("%T", tok)
+		}
 	}
 
-	return respBody, nil
+	if len(all) > 0 {
+		fmt.Printf("%d error(s) reported:\n", len(all))
+		fmt.Printf("  %-5s %-20s %-10s %-10s %-50s\n", "Code", "Raised By", "Location", "Type", "Message")
+		for _, e := range all {
+			fmt.Printf("  %-5s %-20s %-10s %-10s %-50s\n", e.number, e.raisedBy, e.location, e.ofType, e.message)
+		}
+		return fmt.Errorf("%d error(s) reported", len(all))
+	}
+
+	return nil
 }
