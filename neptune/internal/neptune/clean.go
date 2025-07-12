ff222ca86c7c792475a46b9a444bfac1573965f7
Gareth Powell <gareth@ziniki.org>
2025-07-12 22:52:38 +0100

    Cleaned up Neptune

--- neptune/internal/neptune/clean.go
+++ neptune/internal/neptune/clean.go
@@ -1,13 +1,22 @@
 package neptune
 
-import "github.com/aws/aws-sdk-go-v2/service/neptunedata"
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/neptunedata"
+)
 
 type Cleaner struct {
 	svc *neptunedata.Client
 }
 
 func (c *Cleaner) Clean() error {
-	return nil
+	clean := `	MATCH (n)
+				DETACH DELETE n`
+	cleanQuery := neptunedata.ExecuteOpenCypherQueryInput{OpenCypherQuery: aws.String(clean)}
+	_, err := c.svc.ExecuteOpenCypherQuery(context.TODO(), &cleanQuery)
+	return err
 }
 
 func NewCleaner(db string) (*Cleaner, error) {
